0e99aeaca28036546f73360d62b263093d91b708
Thomas Collin <thomas.collin@etu.unilim.fr>
2022-12-29 17:56:16 +0000

    depot

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,22 @@
+import { Entity, Column, PrimaryGeneratedColumn, EntityRepository, Repository } from 'typeorm';
+
+@Entity()
+export class Users {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+
+  @Column({ type: "timestamp", default: () => "CURRENT_TIMESTAMP"})
+  createdAt: Date;
+
+  @Column({ type: "timestamp", default: () => "CURRENT_TIMESTAMP"})
+  updatedAt: Date;
+}
+
+@EntityRepository(Users)
+export class UsersRepository extends Repository<Users> {}
